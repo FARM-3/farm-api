f4ee706536d381247a109b5b6076c7a4b2149079
Arishaba-Peruth <lorisperutha@gmail.com>
2025-09-30 10:20:06 +0300

    updated financialmanagementapp urls

--- api/urls.py
+++ api/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('financialmanagement.urls')),
+    path('api/', include('financialmanagement.urls')),
     path('api/', include('processing.urls')),
 ]
