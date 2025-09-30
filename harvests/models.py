e7932f57a07047885a0eac3cd4d3e552b473834e
Namusobya Edna <nakyosiedna@gmail.com>
2025-09-30 21:09:48 +0300

    repaired conflicts

---
+++ harvests/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Harvests(models.Model):
+    cherry_color= models.AutoField(primary_key=True)
+    date_of_harvest = models.CharField(max_length=100)
+    quantity_harvest = models.CharField(max_length=200)
+    crop_type = models.CharField(max_length=100)
+    block = models.DateField()
+    
+    def __str__(self):
+        return self.cherry_color
+    
