ea0be9c63461ac87bc70c6f0ea02a7bda97c8892
Arishaba-Peruth <lorisperutha@gmail.com>
2025-10-01 11:48:58 +0300

    Added permissions on APIs

--- api/settings.py
+++ api/settings.py
@@ -81,7 +81,7 @@ REST_FRAMEWORK = {
     
     # Require authentication by default (except views with AllowAny)
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     
     # Use JSON renderer (standard for APIs)
