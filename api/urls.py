3bbb1926467a0ec1fe9bbc295e100db3b2e11ea0
Arishaba-Peruth <lorisperutha@gmail.com>
2025-10-01 12:19:18 +0300

    Commit 1

--- api/urls.py
+++ api/urls.py
@@ -28,6 +28,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+     path('api-auth/', include('rest_framework.urls')),
     path('api/finance/', include('financialmanagement.urls')),
     path('api/aggregation/', include('aggregation.urls')),
     path('api/harvests/', include('harvests.urls')),
