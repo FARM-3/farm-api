cbaddb30b5f9eada30854c88f6ec77a5aa7c65e2
Kevin Mukama <kevinmukama16@gmail.com>
2025-10-03 10:29:59 +0300

    Changed farmer model

--- aggregation/models.py
+++ aggregation/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Farmer(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=200) 
-    contact_info = models.CharField(max_length=100)
-    gender = models.CharField(max_length=10)
-    age = models.IntegerField()
+    name = models.CharField(max_length=100)               # Farmer's name
+    location = models.CharField(max_length=200)           # Farmer's location
+    number_of_trees = models.PositiveIntegerField()       # Count of trees (only positive numbers)
+    contact = models.CharField(max_length=100)            # Contact details (phone/email/etc.)
 
     def __str__(self):
         return self.name
