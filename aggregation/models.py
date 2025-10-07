2f1ab2fa27195a3c304ef60db937358ec855bc16
Kevin Mukama <kevinmukama16@gmail.com>
2025-10-07 20:23:36 +0300

    Kevin's commit

--- aggregation/models.py
+++ aggregation/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Farmer(models.Model):
     name = models.CharField(max_length=100)               # Farmer's name
     location = models.CharField(max_length=200)           # Farmer's location
-    num_trees = models.CharField(max_length=100)       # Count of trees (only positive numbers)
+    num_trees = models.CharField(max_length=100, blank=True)       # Count of trees (only positive numbers)
     contact = models.CharField(max_length=100)            # Contact details (phone/email/etc.)
 
     def __str__(self):
--- api/settings.py
+++ api/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 # SECURITY WARNING: don't run with debug turned on in production
 # SECURITY WARNING: don't run with debug turned on in production
-DEBUG = config('DEBUG', default=False, cast=bool) 
+DEBUG = config('DEBUG', default=True, cast=bool) 
 
 if DEBUG:
     # 🌟 LOCAL DEVELOPMENT SETTINGS 🌟
@@ -250,7 +250,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
