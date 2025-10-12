2cb4abd212efe0e76e9449eb16e5e6315aacfcea
Arishaba-Peruth <lorisperutha@gmail.com>
2025-10-13 00:32:25 +0300

    updated the financial management app

--- aggregation/urls.py
+++ aggregation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FarmerRegistrationViewSet, FarmerHarvestViewSet, get_wakiso_parishes
+from .views import FarmerRegistrationViewSet, FarmerHarvestViewSet
 
 router = DefaultRouter()
 router.register(r'Farmer', FarmerRegistrationViewSet)
@@ -8,5 +8,5 @@ router.register(r'FarmerHarvest', FarmerHarvestViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('wakiso-parishes/', get_wakiso_parishes, name='wakiso-parishes'),
+    
 ]
