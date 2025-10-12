bd589709cf69a55f86be58411d39c7e02f6af9ce
Namusobya Edna <nakyosiedna@gmail.com>
2025-10-12 23:59:44 +0300

    created a block model and api

--- harvests/views.py
+++ harvests/views.py
@@ -3,6 +3,11 @@ from django.views.generic import ListView
 from .models import Harvests
 from .serializers import HarvestsSerializer
 from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Block
+from .serializers import BlockSerializer
 
 
 
@@ -16,6 +21,17 @@ class HarvestListView(ListView):
 class HarvestsViewSet(viewsets.ModelViewSet):
     queryset = Harvests.objects.all()
     serializer_class = HarvestsSerializer
+
+
+class BlockViewSet(viewsets.ModelViewSet):
+    """ViewSet for Block management"""
+    queryset = Block.objects.all()
+    serializer_class = BlockSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'block_id'
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
 
     
 
