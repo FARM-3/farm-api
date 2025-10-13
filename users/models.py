bfc8dd23359dc6058d0a630b7baf3f4437f41a03
Kevin Mukama <kevinmukama16@gmail.com>
2025-10-13 23:01:42 +0300

    Removed name attribute

--- users/models.py
+++ users/models.py
@@ -94,12 +94,6 @@ class User(AbstractBaseUser):
         unique=True,
         help_text="Phone number used for login"
     )
-
-    name = models.CharField(
-        max_length=100,
-        blank=True,
-        help_text="User's full name"
-    )
     
     role = models.CharField(
         max_length=20, 
@@ -144,7 +138,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
     
     USERNAME_FIELD = "phone"      # Login with phone instead of username
-    REQUIRED_FIELDS = ["name"]          # Only phone + pin needed for createsuperuser
+    REQUIRED_FIELDS = []          # Only phone + pin needed for createsuperuser
     
     class Meta:
         verbose_name = "User"
