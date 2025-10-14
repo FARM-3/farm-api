099cd976a721e01df218ef871bbe95a7ecc6c50b
Namusobya Edna <nakyosiedna@gmail.com>
2025-10-14 05:29:59 +0300

    commit4

--- aggregation/models.py
+++ aggregation/models.py
@@ -192,8 +192,8 @@ class FarmerRegistration(models.Model):
     #Farmerharvest model
 class FarmerHarvest(models.Model):
     name = models.CharField(max_length=100)               # Farmer's name
-    weight_on_delivery = models.IntegerField(max_length=100, null=True)         # Weight of the harvest
-    weight_after_floating = models.IntegerField(max_length=100, null=True)       # Weight after floating
+    weight_on_delivery = models.IntegerField(null=True)         # Weight of the harvest
+    weight_after_floating = models.IntegerField(null=True)       # Weight after floating
     date_of_delivery = models.CharField(max_length=100, null=True)    # Date of delivery
     grade = models.CharField(max_length=100, null=True)               # Grade of the harvest
     cherry_color = models.CharField(max_length=100, null=True)        # Color of the cherry
