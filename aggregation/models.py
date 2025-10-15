5945e647e69fd24acd045c0d99b342c87369bf4e
Kevin Mukama <kevinmukama16@gmail.com>
2025-10-15 15:28:43 +0300

    reversed back to array field for fertilizer

--- aggregation/models.py
+++ aggregation/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 import random
 import requests
@@ -184,8 +183,8 @@ class FarmerRegistration(models.Model):
     irrigation_source = models.CharField(max_length=100, blank=False)        # Source of irrigation
 
     #agro-chemicals used
-    fertilizers = ArrayField(models.CharField(max_length=100), blank=True, default=list)        # Fertilizers used
-    pesticide = ArrayField(models.CharField(max_length=100), blank=True, default=list)          # Pesticides used
+    fertilizers = models.CharField(max_length=100, blank=False)        # Fertilizers used
+    pesticide = models.CharField(max_length=100, blank=False)          # Pesticides used
 
 
 
