c720a544aaf74a294e7592cf867eacb5e4a47c04
Kevin Mukama <kevinmukama16@gmail.com>
2025-10-28 00:10:37 +0300

    Repaired Aggregation migrations

--- aggregation/migrations/0004_rename_cherry_color_farmerharvest_coffee_type_and_more.py
+++ aggregation/migrations/0004_rename_cherry_color_farmerharvest_coffee_type_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('aggregation', '0003_remove_farmerregistration_other_district_and_more'),
+        ('aggregation', '0001_initial'),
     ]
 
     operations = [
