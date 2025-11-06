decc9d447a52e5a724e3c7293472b1969941365f
Arishaba-Peruth <lorisperutha@gmail.com>
2025-11-06 14:39:35 +0300

    Updated settings.py

--- api/settings.py
+++ api/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
     "django_filters",
